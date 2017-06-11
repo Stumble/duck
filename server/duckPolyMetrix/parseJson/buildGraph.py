0aece1dbac5f0284386b8fd396255fe58d90d31c
Yukai Tu <tuyukai1994@gmail.com>
2017-06-11 13:22:31 -0700

    fix inherited problem

--- server/duckPolyMetrix/parseJson/buildGraph.py
+++ server/duckPolyMetrix/parseJson/buildGraph.py
@@ -1,16 +1,16 @@
 
 def buildInhritedGraph(classesList, classesQueryDic):
+	rootList = []
 	for c in classesList:
 		for inherited in c.inheritedList:
 			if not inherited in classesQueryDic:
 				print "inherited classes not found"
-				continue
-			c.addParentClass(classesQueryDic[inherited])
-			classesQueryDic[inherited].addChildClass(c)
-	rootList = []
+			else:
+				c.addParentClass(classesQueryDic[inherited])
+				classesQueryDic[inherited].addChildClass(c)
 
 	for c in classesList:
-		if not c.inheritedList:
+		if not c.parentClassList:
 			print c.name+" doesn't has parent"
 			rootList.append(c)
 
