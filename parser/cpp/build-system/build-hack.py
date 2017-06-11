f689f2773f2d3f5ea5c8f0c6b3a57ea37f7e86d2
Yumin Xia <me@yumin-xia.com>
2017-06-10 22:51:27 -0700

    add building system hacks

---
+++ parser/cpp/build-system/build-hack.py
@@ -0,0 +1,15 @@
+
+
+
+with open("build.log") as f:
+    content = f.readlines()
+    for line in content:
+        components = line.split()
+        if 'runner' in components[1]:
+            mystr = line[line.find(" [") + 2: line.find("]") ]
+            parameters = mystr.split(',')
+            trimed_parameter = [x[x.find("'") + 1 : -1] for x in parameters]
+            trimed_parameter.append("-emit-ast")
+            ans = " ".join(trimed_parameter)
+            print ans
+            print ""
