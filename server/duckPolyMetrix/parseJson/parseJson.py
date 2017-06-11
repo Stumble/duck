65680ad3fc65e861bbbf406f60c67c46151ea546
Yumin Xia <me@yumin-xia.com>
2017-06-10 21:14:22 -0700

    update JSON format example

--- server/duckPolyMetrix/parseJson/parseJson.py
+++ server/duckPolyMetrix/parseJson/parseJson.py
@@ -19,66 +19,109 @@ methodCallingFuncAttr = 'calling-function'
 attrLinesOfCode = 'linesOfCode'
 attrNumOfMethods = 'numOfMethods'
 
+# example JSON file
+# {
+#   "classes": {
+#     "class testMyOuter::testMyInner::Base": {
+#       "fileds": {
+#         "x": "class int"
+#       },
+#       "methods": {},
+#       "base_list": []
+#     },
+#     "class testMyOuter::testMyInner::Bar": {
+#       "fileds": {
+#         "myDad": "class testMyOuter::testMyInner::Foo"
+#       },
+#       "methods": {
+#         "test(const class testMyOuter::testMyInner::Foo *const)": {
+#           "linesOfCode": 8,
+#           "parameter": [
+#             "shit"
+#           ],
+#           "calling-function": [],
+#           "run-time-invokes": 0
+#         },
+#         "test()": {
+#           "linesOfCode": 7,
+#           "parameter": [],
+#           "calling-function": [],
+#           "run-time-invokes": 0
+#         }
+#       },
+#       "base_list": [
+#         "class testMyOuter::testMyInner::Foo",
+#         "class testMyOuter::testMyInner::Base"
+#       ]
+#     },
+#     "class testMyOuter::testMyInner::Foo": {
+#       "fileds": {
+#         "xx": "class int",
+#         "id": "class int"
+#       },
+#       "methods": {
+#         "print(std::string)": {
+#           "linesOfCode": 5,
+#           "parameter": [
+#             "str"
+#           ],
+#           "calling-function": [],
+#           "run-time-invokes": 0
+#         }
+#       },
+#       "base_list": []
+#     }
+#   }
+# }
+
+
 def parse(resultJson, projectDir):
-	#try:
-		print "start parse"
-		with open(resultJson) as data_file:    
-			data = json.load(data_file)
-			#{u'classes': {u'foo': {u'inherited list': [u'BaseA', u'BaseB'],
-			#						u'methods': {u'addAandB': {u'run-time invokes': 10,
-			#													u'linesOfCode': 38, 
-			#													u'parameter': [u'Int',
-			#																   u'Bool',
-			#																   u'String'],
-			#													u'calling-function': [u'Bar.subAandB', 
-			#																		  u'Pc.test']}}}, 
-			#			   u'bar': {u'inherited list': [], 
-			#			   			u'methods': [{u'init': {u'run-time invokes': 0, 
-			#			   									u'linesOfCode': 0, 
-            #     											u'calling-function':[],
-			#			   									u'parameter': []}}]}}}
-			classesList = []
-			classesQueryDic = {}
+        #try:
+                print "start parse"
+                with open(resultJson) as data_file:
+                        data = json.load(data_file)
+                        classesList = []
+                        classesQueryDic = {}
 
 
-			classesDic = data[classesAttr]
-			print classesDic
-			for className in classesDic:
-				attrDic = classesDic[className]
-				c = JClass(className)
+                        classesDic = data[classesAttr]
+                        print classesDic
+                        for className in classesDic:
+                                attrDic = classesDic[className]
+                                c = JClass(className)
 
-				c.attrDic[attrLinesOfCode] = 0
-				c.attrDic[attrNumOfMethods] = 0
+                                c.attrDic[attrLinesOfCode] = 0
+                                c.attrDic[attrNumOfMethods] = 0
 
-				if inHeritedListAttr in attrDic:
-					for inheritedClass in attrDic[inHeritedListAttr]:
-						c.addInheritedClass(inheritedClass)
+                                if inHeritedListAttr in attrDic:
+                                        for inheritedClass in attrDic[inHeritedListAttr]:
+                                                c.addInheritedClass(inheritedClass)
 
-				if methodsAttr in attrDic:
-					methodsDic = attrDic[methodsAttr]
-					for methodName in methodsDic:
-						
-						c.attrDic[attrNumOfMethods] += 1						
-						methodAttrDic = methodsDic[methodName]
-						m = JMethod(methodName, className)
-						if methodRunTimeAttr in methodAttrDic:
-							m.setRunTimeInvokes(methodAttrDic[methodRunTimeAttr])
-						if methodLOCAttr in methodAttrDic:
-							m.setLineOfCode(methodAttrDic[methodLOCAttr])
-							c.attrDic[attrLinesOfCode] += methodAttrDic[methodLOCAttr]
-						if methodParaAttr in methodAttrDic:
-							parameterList = methodAttrDic[methodParaAttr]
-							for paraType in parameterList:
-								m.addParameters(paraType)
-						if methodCallingFuncAttr in methodAttrDic:
-							callingList = methodAttrDic[methodCallingFuncAttr]
-							for callingfunc in callingList:
-								m.addCallingFunctionName(callingfunc)
-						c.addMethod(m)
-				classesList.append(c)
-				classesQueryDic[className] = c
+                                if methodsAttr in attrDic:
+                                        methodsDic = attrDic[methodsAttr]
+                                        for methodName in methodsDic:
 
-			print classesList
+                                                c.attrDic[attrNumOfMethods] += 1
+                                                methodAttrDic = methodsDic[methodName]
+                                                m = JMethod(methodName, className)
+                                                if methodRunTimeAttr in methodAttrDic:
+                                                        m.setRunTimeInvokes(methodAttrDic[methodRunTimeAttr])
+                                                if methodLOCAttr in methodAttrDic:
+                                                        m.setLineOfCode(methodAttrDic[methodLOCAttr])
+                                                        c.attrDic[attrLinesOfCode] += methodAttrDic[methodLOCAttr]
+                                                if methodParaAttr in methodAttrDic:
+                                                        parameterList = methodAttrDic[methodParaAttr]
+                                                        for paraType in parameterList:
+                                                                m.addParameters(paraType)
+                                                if methodCallingFuncAttr in methodAttrDic:
+                                                        callingList = methodAttrDic[methodCallingFuncAttr]
+                                                        for callingfunc in callingList:
+                                                                m.addCallingFunctionName(callingfunc)
+                                                c.addMethod(m)
+                                classesList.append(c)
+                                classesQueryDic[className] = c
+
+                        print classesList
 
 
 
@@ -100,14 +143,14 @@ def parse(resultJson, projectDir):
 #							if classMethod.name == funcName:
 #								m.addCallingFunction(classMethod)
 
-			baseClasses = buildTree.buildInhritedTree(classesList, classesQueryDic)
+                        baseClasses = buildTree.buildInhritedTree(classesList, classesQueryDic)
 
-			outputJson.outputFlareInheritedJson(baseClasses, attrLinesOfCode, projectDir+"/LOCInherited.json")
-			outputCSV.outputInheritedCSV(baseClasses, attrLinesOfCode, projectDir+"/LOCInherited.csv")
+                        outputJson.outputFlareInheritedJson(baseClasses, attrLinesOfCode, projectDir+"/LOCInherited.json")
+                        outputCSV.outputInheritedCSV(baseClasses, attrLinesOfCode, projectDir+"/LOCInherited.csv")
 
-			print baseClasses
+                        print baseClasses
 
 
-	#except:
+        #except:
 #		print "exception occurred"
 #		raise
