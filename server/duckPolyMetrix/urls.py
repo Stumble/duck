fcdf1b0c271fdd38c259544c379ea5eaca746102
Yukai Tu <tuyukai1994@gmail.com>
2017-06-10 23:57:05 -0700

    add option to all graph

--- server/duckPolyMetrix/urls.py
+++ server/duckPolyMetrix/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     url(r'^index', views.index, name='index'),
     url(r'^collapsibletree/(?P<matrix>\w+)/(?P<query>\w+)/$', views.collapsibletree, name='collapsibletree'),
-    url(r'^zoomablesunburst', views.zoomablesunburst, name='zoomablesunburst'),
-    url(r'^edgebundling', views.edgebundling, name='edgebundling'),
-    url(r'^circlepacking', views.circlepacking, name='circlepacking'),
+    url(r'^zoomablesunburst/(?P<matrix>\w+)/(?P<query>\w+)/$', views.zoomablesunburst, name='zoomablesunburst'),
+    url(r'^edgebundling/(?P<matrix>\w+)/(?P<query>\w+)/$', views.edgebundling, name='edgebundling'),
+    url(r'^circlepacking/(?P<matrix>\w+)/(?P<query>\w+)/$', views.circlepacking, name='circlepacking'),
     url(r'^getResult/(?P<projectName>\w+)/$', views.getResult, name='getResult'),
     url(r'^parseProject/(?P<projectName>\w+)/$', views.parseProject, name='parseProject'),
 ]
